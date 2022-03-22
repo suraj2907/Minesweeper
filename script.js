ec82af81b44b0297a8045d5ba9bfdbbcadb99620
suraj2907 <surajjawrani2022@gmail.com>
2022-03-22 21:01:17 +0530

    added mouse handling

---
+++ script.js
@@ -0,0 +1,231 @@
+// defining variable which we will use in various places
+var components = {
+    num_of_rows : 12,
+    num_of_cols : 24,
+    num_of_bombs : 55,
+    bomb : '💣',
+    alive : true,
+    colors : {1: 'blue', 2: 'green', 3: 'red', 4: 'purple', 5: 'maroon', 6: 'turquoise', 7: 'black', 8: 'grey'}
+}
+
+//invoking start function and starts new game
+window.addEventListener('load', function() {
+    document.getElementById('lost').style.display="none";
+    startGame();
+});
+
+//it will invoke start() to place bombs
+function startGame() {
+    components.bombs = placeBombs();
+    document.getElementById('field').appendChild(createTable());
+}
+
+// it invoke placesinglebomb() to place the bomb
+function placeBombs() {
+    var i, rows = [];
+    
+    for (i=0; i<components.num_of_bombs; i++) {
+        placeSingleBomb(rows);
+    }
+    return rows;
+} 
+
+//it will place bombs
+function placeSingleBomb(bombs) {
+
+    var nrow, ncol, row, col;
+    nrow = Math.floor(Math.random() * components.num_of_rows);
+    ncol = Math.floor(Math.random() * components.num_of_cols);
+    row = bombs[nrow];
+    
+    if (!row) {
+        row = [];
+        bombs[nrow] = row;
+    }
+    
+    col = row[ncol];
+    
+    if (!col) {
+        row[ncol] = true;
+        return
+    } 
+    else {
+        placeSingleBomb(bombs);
+    }
+}
+
+//it will return the cell id
+function cellID(i, j) {
+    return 'cell-' + i + '-' + j;
+}
+
+//it will create table of 12 rows and 24 coloums
+function createTable() {
+    var table, row, td, i, j;
+    table = document.createElement('table');
+    
+    for (i=0; i<components.num_of_rows; i++) {
+        row = document.createElement('tr');
+        for (j=0; j<components.num_of_cols; j++) {
+            td = document.createElement('td');
+            td.id = cellID(i, j);
+            row.appendChild(td);
+            addCellListeners(td, i, j);
+        }
+        table.appendChild(row);
+    }
+    return table;
+}
+
+//
+function addCellListeners(td, i, j) {
+    td.addEventListener('mousedown', function(event) {
+        if (!components.alive) {
+            return;
+        }
+        components.mousewhiches += event.which;
+        if (event.which === 3) {
+            return;
+        }
+        if (this.flagged) {
+            return;
+        }
+        this.style.backgroundColor = 'lightGrey';
+    });
+
+    td.addEventListener('mouseup', function(event) {
+      
+        if (!components.alive) {
+            return;
+        }
+
+        if (this.clicked && components.mousewhiches == 4) {
+            performMassClick(this, i, j);
+        }
+
+        components.mousewhiches = 0;
+        
+        if (event.which === 3) {
+           
+            if (this.clicked) {
+                return;
+            }
+            if (this.flagged) {
+                this.flagged = false;
+                this.textContent = '';
+            } else {
+                this.flagged = true;
+                this.textContent = components.flag;
+            }
+
+            event.preventDefault();
+            event.stopPropagation();
+          
+            return false;
+        } 
+        else {
+            handleCellClick(this, i, j);
+        }
+    });
+
+    td.oncontextmenu = function() { 
+        return false; 
+    };
+}
+
+//it is handling while clicking into cell
+function handleCellClick(cell, i, j) {
+    if (!components.alive) {
+        return;
+    }
+
+    if (cell.flagged) {
+        return;
+    }
+
+    cell.clicked = true;
+
+    if (components.bombs[i][j]) {
+        cell.style.color = 'red';
+        cell.textContent = components.bomb;
+        gameOver();
+        
+    }
+    else {
+        cell.style.backgroundColor = 'lightGrey';
+        num_of_bombs = adjacentBombs(i, j);
+        if (num_of_bombs) {
+            cell.style.color = components.colors[num_of_bombs];
+            cell.textContent = num_of_bombs;
+        } 
+        else {
+            clickAdjacentBombs(i, j);
+        }
+    }
+}
+
+//placing bombs adjacent to the no.'s 
+function adjacentBombs(row, col) {
+    var i, j, num_of_bombs;
+    num_of_bombs = 0;
+
+    for (i=-1; i<2; i++) {
+        for (j=-1; j<2; j++) {
+            if (components.bombs[row + i] && components.bombs[row + i][col + j]) {
+                num_of_bombs++;
+            }
+        }
+    }
+    return num_of_bombs;
+}
+
+// checking and placing the flags
+function adjacentFlags(row, col) {
+    var i, j, num_flags;
+    num_flags = 0;
+
+    for (i=-1; i<2; i++) {
+        for (j=-1; j<2; j++) {
+            cell = document.getElementById(cellID(row + i, col + j));
+            if (!!cell && cell.flagged) {
+                num_flags++;
+            }
+        }
+    }
+    return num_flags;
+}
+
+function clickAdjacentBombs(row, col) {
+    var i, j, cell;
+    
+    for (i=-1; i<2; i++) {
+        for (j=-1; j<2; j++) {
+            if (i === 0 && j === 0) {
+                continue;
+            }
+            cell = document.getElementById(cellID(row + i, col + j));
+            if (!!cell && !cell.clicked && !cell.flagged) {
+                handleCellClick(cell, row + i, col + j);
+            }
+        }
+    }
+}
+
+function performMassClick(cell, row, col) {
+    if (adjacentFlags(row, col) === adjacentBombs(row, col)) {
+        clickAdjacentBombs(row, col);
+    }
+}
+
+//display game over
+function gameOver() {
+    components.alive = false;
+    document.getElementById('lost').style.display="block";
+    
+}
+
+//reload the page 
+function reload(){
+    window.location.reload();
+}
+
